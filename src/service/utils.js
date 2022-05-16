135f1cbca0a6c8131932068ed9accad425a30707
hackathonmadeeasy <105452014+hackathonmadeeasy@users.noreply.github.com>
2022-05-16 22:52:47 +0530

    updated country

--- src/service/utils.js
+++ src/service/utils.js
@@ -3,11 +3,11 @@ export const RESOUREC_URL = "https://pronunciation-web.azurewebsites.net/";
 export const Country = [
   { key: "USA", value: "en-US" },
   { key: "South Africa", value: "zu-ZA" },
-  { key: "France", value: "fr-FR" },
+  { key: "INDIA", value: "en-IN" },
 ];
 
 export const CountryVoiceName = {
   "en-US": "en-US-JennyNeural",
   "zu-ZA": "zu-ZA-ThandoNeural",
-  "fr-FR": "fr-FR-YvetteNeural",
+  "en-IN": "en-IN-NeerjaNeural",
 };
